6fb7bb3b7c3d7048eadc6fe98262cd859278b882
wangyiwen <514094893@qq.com>
2017-03-10 16:43:21 +0800

    测试配置文件（二）

--- jeve/service-demo-A/src/main/java/com/jeve/demo/a/DemoAApplication.java
+++ jeve/service-demo-A/src/main/java/com/jeve/demo/a/DemoAApplication.java
@@ -1,13 +1,13 @@
 package com.jeve.demo.a;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DemoAApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new SpringApplicationBuilder(DemoAApplication.class).web(true).run(args);
 	}
 
 }
